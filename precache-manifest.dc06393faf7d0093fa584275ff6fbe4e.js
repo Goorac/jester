72c681f1ec4482805b1b491032511e930f97d3cc
Goorac <82958803+Goorac@users.noreply.github.com>
2021-09-23 19:26:22 +0530

    Add files via upload

---
+++ precache-manifest.dc06393faf7d0093fa584275ff6fbe4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b56a85476b1a98a34e4",
+    "url": "/static/css/main.e885a1d5.chunk.css"
+  },
+  {
+    "revision": "4b56a85476b1a98a34e4",
+    "url": "/static/js/main.dcd77230.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "286fd0be1edea430f092",
+    "url": "/static/css/2.0afdc7f2.chunk.css"
+  },
+  {
+    "revision": "286fd0be1edea430f092",
+    "url": "/static/js/2.7bf24432.chunk.js"
+  },
+  {
+    "revision": "20811a374fd7ada2bd18111ba423abfb",
+    "url": "/index.html"
+  }
+];
